1518dd1dc804a1f81779bcbd59dfcfc93a4fc4d3
Samantha <lovelychick1111@yahoo.com>
2018-03-11 20:33:11 -0400

    create array [] of objects {} for possible friend match

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,32 +1,41 @@
-friendsData = [
+
+// I am creating an array [] of objects {} for the possible matches for the person looking for a friend
+
+var friendMatch = [
 	{
-	friendName: '',
-	friendPhoto: '',
-	questionOne: '',
-	questionTwo: '',
-	questionThree: '',
-	questionFour: '',
-	questionFive: '',
-	questionSix: '',
-	questionSeven: '',
-	questionEight: '',
-	questionNine: '',
-	questionTen: '',
-	}
+		"name":"Ahmed",
+		"photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+		"scores":[
+				5,
+				1,
+				4,
+				4,
+				5,
+				1,
+				2,
+				5,
+				4,
+				1
+			]
+	},
+	{
+		"name":"Samantha",
+		"photo":"https://www.pexels.com/search/girl/",
+		"scores":[
+				2,
+				1,
+				3,
+				1,
+				2,
+				1,
+				5,
+				3,
+				2,
+				1
+			]
+	},
 
 ]
-	
-
-module.exports = friendsData;
-// console.log(appData.scores[2]);
-
-// var answerOne = 'yes';
-// var answerTwo = 'no';
-
-// console.log(appData.scores.push(answerOne));
-
-// console.log(appData.scores);
 
 
-
-
+module.exports = friendMatch; // This exports the array [] only. So if there were other pieces of code here it wouldn't be visible to other files in your app
